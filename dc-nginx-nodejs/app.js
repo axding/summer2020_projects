127cb14baa9025aa4e21a7364e98c2bd05874431
Amanda Ding <axding@usc.edu>
2020-06-01 21:59:59 -0500

    changes to Dockerfile and docker-compose.yml

--- dc-nginx-nodejs/app.js
+++ dc-nginx-nodejs/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World from me!');
 });
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app!');
 });
